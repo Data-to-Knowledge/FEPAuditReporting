921cda3ff3f6fee18e8dec3e8bd3acd30d4a563c
Katie Simpson <Katie.Simpson@ecan.govt.nz>
2019-04-18 16:56:31 +1200

    Update FEP Audit Monthly Report.py

--- FEP Audit Monthly Report.py
+++ FEP Audit Monthly Report.py
@@ -120,31 +120,48 @@ AuditFarm = AuditFilter[['PrimaryFarmType','Grade']]
 
 
 # Create Pivot
-x = Audit.pivot_table(Audit,
-                  index=['Zone','Audit Submission Month'],
-                #  values=['gsID'],
-                  columns=['Grade'],
-                  aggfunc=[len],
-                  fill_value=0
-                  )
+#x = Audit.pivot_table(Audit,
+#                  index=['Zone','Audit Submission Month'],
+#                #  values=['gsID'],
+#                  columns=['Grade'],
+#                  aggfunc=[len],
+#                  fill_value=0,
+#                  dropna = False
+#                  )
+#
+#x.query('Zone == ["Zone Ashburton "]')
+#'Manager == ["Debra Henley"]'
+#x= AuditGrade.pivot_table(AuditGrade,
+#                  index=['Zone'],
+#                  columns=['Grade'],
+#                  aggfunc=[len],
+#                  fill_value=0,
+#                  dropna = False
+#                  )
+#
+#y= AuditFarm.pivot_table(AuditFarm,
+#                  index=['PrimaryFarmType'],
+#                  columns=['Grade'],
+#                  aggfunc=[len],
+#                  fill_value=0
+#                  )
 
-x.query('Zone == ["Zone Ashburton "]')
-'Manager == ["Debra Henley"]'
-x= AuditGrade.pivot_table(AuditGrade,
-                  index=['Zone'],
-                  columns=['Grade'],
-                  aggfunc=[len],
-                  fill_value=0
-                  )
+ZoneGrades = Audit.groupby(['Audit Submission Month','Zone', 'Grade'])['Grade'].aggregate('count').unstack()
+ZoneGrades.fillna(0, inplace= True)
+ZoneGrades[list("ABCD")] = ZoneGrades[list("ABCD")].astype(int)
+print(ZoneGrades)
 
-y= AuditFarm.pivot_table(AuditFarm,
-                  index=['PrimaryFarmType'],
-                  columns=['Grade'],
-                  aggfunc=[len],
-                  fill_value=0
-                  )
 
-https://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
+FarmTypeGrades = Audit.groupby(['PrimaryFarmType','Audit Submission Month', 'Grade'])['Grade'].aggregate('count').unstack()
+FarmTypeGrades.fillna(0, inplace= True)
+FarmTypeGrades[list("ABCD")] = FarmTypeGrades[list("ABCD")].astype(int)
+print(FarmTypeGrades)
+x= FarmTypeGrades.Index(['April'])
+x = FarmTypeGrades.reset_index()
+x.reset_index(level='Audit Submission Month')
+
+
+# https://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
 x.to_csv('test.csv')
 
 Audit.groupby(['Zone'])['Grade'].count()
